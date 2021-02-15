cf3a3d74532c9c11a4c169146ac62b5805cb91e9
Roland Levy <rolandjlevy@gmail.com>
2021-02-15 20:55:40 +0000

    Adapting to OOP

--- script.js
+++ script.js
@@ -1,46 +1,24 @@
+import Percussion from './src/Percussion.js';
+
 const $ = (selector) => document.querySelector(selector);
 const $$ = (selector) => document.querySelectorAll(selector);
 
-const drums = {
-  "a": "clap",
-  "s": "hihat",
-  "d": "kick",
-  "f": "openhat",
-  "g": "boom",
-  "h": "ride",
-  "j": "snare",
-  "k": "tom",
+const keyMap = {
+  "a": "hihat", 
+  "s": "openhat",
+  "d": "ride",
+  "f": "snare",
+  "g": "kick",
+  "h": "tom",
+  "j": "boom",
+  "k": "clap",
   "l": "tink"
 }
 
-function addDrum(key, value) {
-  return `
-  <section id="${key}" class="key">
-    <audio src="sounds/${value}.wav"></audio>
-    <h3>${key}</h3>
-    <p>${value}</p>
-  </section>`;
-}
+const percussion = [];
 
-$('.drums').innerHTML = Object.entries(drums).map(([key, value]) => addDrum(key, value)).join('');
-
-function playsound(key) {
-  const audio = $(`section#${key} > audio`);
-  const btn = $(`section#${key}`);
-  btn.classList.add('playing');
-  if (!audio) return;
-  audio.currentTime = 0;
-  audio.play();
-}
-
-function removeTransition(e) {
-  if (e.propertyName !== 'transform') return;
-  this.classList.remove('playing');
-}
-
-$$('.key').forEach(key => {
-  key.addEventListener('transitionend', removeTransition);
-  key.addEventListener('click', (e) => playsound(e.currentTarget.id) );
+Object.entries(keyMap).forEach(([key, name]) => {
+  percussion.push(new Percussion(key, name));
 });
 
-window.addEventListener('keydown', (e) => playsound(e.key) );
+// window.addEventListener('keydown', (e) => playsound(e, e.key) );
---
+++ src/Percussion.js
@@ -0,0 +1,50 @@
+export default class Percussion {
+  constructor(key, name) {
+    this.key = key;
+    this.name = name;
+    this.add();
+  }
+  $(selector) {
+    return document.querySelector(selector);
+  }
+  add() {
+    this.section = document.createElement('section');
+    this.section.id = this.key;
+    this.section.classList.add('key');
+    this.audio = document.createElement('audio'); 
+    this.audio.src = `sounds/${this.name}.wav`;
+    this.section.appendChild(this.audio);
+    const h3 = document.createElement('h3'); 
+    h3.textContent = this.key;
+    this.section.appendChild(h3);
+    const p = document.createElement('p'); 
+    p.textContent = this.name;
+    this.section.appendChild(p);
+    this.section.addEventListener('click', (e) => {
+      this.playAudio();
+      this.section.classList.add('playing');
+    });
+    this.section.addEventListener('transitionend', (e) => this.removePlayingState());
+    this.$('.drums').appendChild(this.section);
+  }
+  playAudio() {
+    if (!this.audio) return;
+    this.audio.currentTime = 0;
+    this.audio.play();
+  }
+  removePlayingState() {
+    if (!this.section) return;
+    if (this.section.className.includes('playing')) {
+      this.section.classList.remove('playing');
+    }
+  }
+
+  // addHtml() {
+  //   return `
+  //   <section id="${this.key}" class="key">
+  //     <audio src="sounds/${this.name}.wav"></audio>
+  //     <h3>${this.key}</h3>
+  //     <p>${this.name}</p>
+  //   </section>`;
+  // }
+}
